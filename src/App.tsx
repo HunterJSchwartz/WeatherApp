54072604b0a4fb7a2560693bf848b1615a3c60b1
HunterJSchwartz <hunterjschwartz@gmail.com>
2023-05-23 02:37:55 -0400

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [condition, setCondition] = useState("");
+
+  useEffect(() => {
+    if (condition !== "") {
+    }
+  }, [condition]);
+
+  async function Testing() {
+    const baseURL = "http://api.weatherapi.com/v1/current.json?";
+    const params = new URLSearchParams({
+      key: "09cedaf909184f7e9cb51126232305",
+      q: "austin",
+    });
+    try {
+      fetch(baseURL + params)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data.current);
+          var day = data.current.is_day == 1 ? "day/" : "night/";
+          setCondition(
+            "./images/weather/" +
+              day +
+              data.current.condition.icon.split("/").at(-1).split(".png")[0] +
+              ".svg"
+          );
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <h1 className="header-text">Weather App</h1>
       </header>
-    </div>
+      <main className="hero-container">
+        <p>Testing</p>
+        <button onClick={Testing}>Testing</button>
+        <img src={condition} />
+      </main>
+    </>
   );
 }
 
